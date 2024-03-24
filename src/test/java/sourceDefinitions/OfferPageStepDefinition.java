9f2c79321612b6b2e8122c01cfa2ff3c04fee865
slink007 <411289114+slink007@users.noreply.github.com>
2024-03-24 10:48:04 -0400

    Remove setup from 'Then' implementation and place into a separate method to enfore single responsibility.

--- src/test/java/sourceDefinitions/OfferPageStepDefinition.java
+++ src/test/java/sourceDefinitions/OfferPageStepDefinition.java
@@ -22,16 +22,22 @@ public class OfferPageStepDefinition {
     
     @Then("user searched for same short name {string} on offers page to check if product exists")
 	public void user_searched_for_same_short_name_on_offers_page_to_check_if_product_exists(String shortName) throws InterruptedException {
-    	testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
-        Set<String> handles = testContextSetup.driver.getWindowHandles();
-        Iterator<String> i1 = handles.iterator();
-        String parentWindow = i1.next();
-        String childWindow = i1.next();
-        testContextSetup.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        testContextSetup.driver.switchTo().window(childWindow);
-        
+    	switchToOffersPage();
         testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
         String offerPageProductName = testContextSetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
         Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
 	}
+    
+    public void switchToOffersPage() {
+    	if(!testContextSetup.driver.getCurrentUrl().
+    			equalsIgnoreCase("https://rahulshettyacademy.com/seleniumPractise/#/offers")) {
+    		testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+            Set<String> handles = testContextSetup.driver.getWindowHandles();
+            Iterator<String> i1 = handles.iterator();
+            String parentWindow = i1.next();
+            String childWindow = i1.next();
+            testContextSetup.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+            testContextSetup.driver.switchTo().window(childWindow);
+    	}
+    }
 }
