123b5df2895a23274cf86debbaee4dbf88959f87
slink007 <411289114+slink007@users.noreply.github.com>
2024-03-24 10:41:30 -0400

    Finish all step definitions, verify progam works, and update gitignore to not track test output.

--- src/test/java/sourceDefinitions/LandingPageStepDefinition.java
+++ src/test/java/sourceDefinitions/LandingPageStepDefinition.java
@@ -6,23 +6,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import utils.TestContextSetup;
 
 public class LandingPageStepDefinition {
 	
 	public WebDriver driver;
-	public String landingPageProductName;
+	TestContextSetup testContextSetup;
+    
+    public LandingPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+    }
 	
 	@Given("user is on GreenKart landing page")
 	public void user_is_on_green_kart_landing_page() {
-	    driver = new FirefoxDriver();
-	    driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-	    driver.manage().window().maximize();
+		testContextSetup.driver = new FirefoxDriver();
+		testContextSetup.driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		testContextSetup.driver.manage().window().maximize();
 	}
 	
 	@When("user searched with short name {string} and extracted actual product name")
 	public void user_searched_with_short_name_and_extracted_actual_product_name(String shortName) throws InterruptedException {
-		driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+		testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
         Thread.sleep(2000);
-        landingPageProductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        testContextSetup.landingPageProductName = testContextSetup.driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
 	}
 }
--- src/test/java/sourceDefinitions/OfferPageStepDefinition.java
+++ src/test/java/sourceDefinitions/OfferPageStepDefinition.java
@@ -1,5 +1,37 @@
 package sourceDefinitions;
 
-public class OfferPageStepDefinition {
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
+import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.Then;
+import utils.TestContextSetup;
+
+public class OfferPageStepDefinition {
+	
+	public String offerPageProductName;
+    TestContextSetup testContextSetup;
+    
+    public OfferPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+    }
+    
+    @Then("user searched for same short name {string} on offers page to check if product exists")
+	public void user_searched_for_same_short_name_on_offers_page_to_check_if_product_exists(String shortName) throws InterruptedException {
+    	testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+        Set<String> handles = testContextSetup.driver.getWindowHandles();
+        Iterator<String> i1 = handles.iterator();
+        String parentWindow = i1.next();
+        String childWindow = i1.next();
+        testContextSetup.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        testContextSetup.driver.switchTo().window(childWindow);
+        
+        testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        String offerPageProductName = testContextSetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
+        Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
+	}
 }
