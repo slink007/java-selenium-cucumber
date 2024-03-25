25f24d1175cfba7b32b8dc90c64e970676c84187
slink007 <41128914+slink007@users.noreply.github.com>
2024-03-25 15:38:25 -0400

    Add a hook to close the browser when the test scenario is concluded.

---
+++ src/test/java/sourceDefinitions/Hooks.java
@@ -0,0 +1,20 @@
+package sourceDefinitions;
+
+import java.io.IOException;
+
+import io.cucumber.java.After;
+import utils.TestContextSetup;
+
+public class Hooks {
+	
+	TestContextSetup testContextSetup;
+	
+	public Hooks(TestContextSetup testContextSetup) {
+		this.testContextSetup = testContextSetup;
+	}
+	
+	@After
+	public void AfterScenario() throws IOException {
+		testContextSetup.testBase.WebDriverManager().quit();
+	}
+}
