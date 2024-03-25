6eb8eff5edab06bb5e93441d22c9ebcadf25b86e
slink007 <41128914+slink007@users.noreply.github.com>
2024-03-25 17:05:58 -0400

    Update test runner to execute tests in parallel.

--- src/test/java/options/TestNGRunner.java
+++ src/test/java/options/TestNGRunner.java
@@ -1,5 +1,7 @@
 package options;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -7,4 +9,9 @@ import io.cucumber.testng.CucumberOptions;
                  monochrome=true)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 	
+	@Override
+	@DataProvider(parallel=true)
+	public Object [][] scenarios() {
+		return super.scenarios();
+	}
 }
