82a26943e00ceefc2adc0a254cf6c859ecb911c7
slink007 <41128914+slink007@users.noreply.github.com>
2024-03-30 19:11:46 -0400

    Update TestBase to allow for web browser name to be passed from command line.

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -14,13 +14,16 @@ public class TestBase {
 	public String url;
 
 	public WebDriver WebDriverManager() throws IOException {
-		FileInputStream fis = new FileInputStream(
-				System.getProperty("user.dir") + "/src/test/resources/global.properties");
-		Properties prop = new Properties();
-		prop.load(fis);
-
 		if (driver == null) {
-			if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "/src/test/resources/global.properties");
+			Properties prop = new Properties();
+			prop.load(fis);
+			String browser_prop = prop.getProperty("browser");
+			String browser_mvn = System.getProperty("browser");
+			String browser = browser_mvn != null ? browser_mvn : browser_prop;
+
+			if (browser.equalsIgnoreCase("firefox")) {
 				driver = new FirefoxDriver();
 			} else {
 				driver = new ChromeDriver();
