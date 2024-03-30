8cd1d379676150ad2775620adf80f89bf9b6db33
slink007 <41128914+slink007@users.noreply.github.com>
2024-03-30 15:42:19 -0400

    Add support for extent reports to contain screen shots of failed tests.

--- src/test/java/sourceDefinitions/Hooks.java
+++ src/test/java/sourceDefinitions/Hooks.java
@@ -1,8 +1,16 @@
 package sourceDefinitions;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utils.TestContextSetup;
 
 public class Hooks {
@@ -16,5 +24,15 @@ public class Hooks {
 	@After
 	public void AfterScenario() throws IOException {
 		testContextSetup.testBase.WebDriverManager().quit();
+	}
+	
+	@AfterStep
+	public void AddScreenshot(Scenario scenario) throws IOException {
+		if(scenario.isFailed()) {
+			WebDriver driver = testContextSetup.testBase.WebDriverManager();
+			File sourcepath = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			byte[] fileContent = FileUtils.readFileToByteArray(sourcepath);
+			scenario.attach(fileContent, "image/png", "image");
+		}
 	}
 }
