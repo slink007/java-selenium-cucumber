0651952ebb7d693c161aacb62e699b8c89949eb9
slink007 <41128914+slink007@users.noreply.github.com>
2024-03-30 14:53:44 -0400

    Add extent reports along with built-in html, json, and xml reports.

--- src/test/java/options/TestNGRunner.java
+++ src/test/java/options/TestNGRunner.java
@@ -6,7 +6,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features", glue="sourceDefinitions", 
-                 monochrome=true)  // Add , tags="@PlaceOrder" to only run that scenario
+                 monochrome=true, tags="@PlaceOrder or @OffersPage",
+                 plugin={"html:target/cucumber.html", "json:target/cucumber.json",
+                		 "junit:target/cucumber.xml",
+                		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})  // Add , tags="@PlaceOrder" to only run that scenario
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 	
 	@Override
